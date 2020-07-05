e7aff337d058d3becab8c545ce2c441684816bc3
o2o2 <37136770+eunbinhyun@users.noreply.github.com>
2020-07-05 19:35:01 +0900

    Update 스택수열.py

--- BkJ/스택수열.py
+++ BkJ/스택수열.py
@@ -13,17 +13,14 @@ i = 1
 while j < len(data):
     if stack and stack[-1] == data[j]:
         stack.pop()
-        # print("-")
         answer += "-\n"
         j += 1
     elif stack and stack[-1] > data[j]:
-        # print("NO")
         answer = "NO"
         break
     else:
         stack.append(i)
         i += 1
-        # print("+")
         answer += "+\n"
 
 print(answer)
