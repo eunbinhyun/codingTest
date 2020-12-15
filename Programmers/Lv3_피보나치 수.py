a49e01811a14b25136918bb0de220ddcb5ac1fa4
o2o2 <37136770+eunbinhyun@users.noreply.github.com>
2020-12-16 00:00:48 +0900

    Create Lv3_피보나치 수.py

---
+++ Programmers/Lv3_피보나치 수.py
@@ -0,0 +1,12 @@
+def fibonacchi(n):
+    memo = [0] * (n + 1)
+    memo[0] = 0
+    memo[1] = 1
+
+    for i in range(2, n + 1):
+        memo[i] = memo[i-1] + memo[i-2]     
+    return memo[-1]
+    
+def solution(n):
+    DIVISOR = 1234567
+    return fibonacchi(n) % DIVISOR
