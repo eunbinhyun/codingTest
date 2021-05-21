4199de7698a1573f4496f6a5acad8aff16670602
EunBin Hyun <dmsqlsgus@naver.com>
2021-05-21 19:45:22 +0900

    Create 4. 점수 계산.js

---
+++ 자바스크립트 알고리즘 문제풀이/섹션2/4. 점수 계산.js
@@ -0,0 +1,16 @@
+  function solution(arr) {
+    let answer = 0;
+    let cnt = 0;
+    for (const score of arr) {
+      if (score === 1) {
+        cnt++;
+        answer += cnt;
+      } else {
+        cnt = 0;
+      }
+    }
+    return answer;
+  }
+
+  let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+  console.log(solution(arr));
