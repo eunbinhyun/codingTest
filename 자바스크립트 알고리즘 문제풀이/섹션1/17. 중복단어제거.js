e63f3f55aeaecf2b74006be5db6b81483a4a46b8
EunBin Hyun <dmsqlsgus@naver.com>
2021-05-29 22:41:37 +0900

    Create 17. 중복단어제거.js

---
+++ 자바스크립트 알고리즘 문제풀이/섹션1/17. 중복단어제거.js
@@ -0,0 +1,8 @@
+  function solution(arr) {
+      let answer = '';
+      let set1 = new Set(arr);
+
+      return [...set1]
+    }
+
+    console.log(solution(['good', 'time', 'good', 'time', 'student']))
