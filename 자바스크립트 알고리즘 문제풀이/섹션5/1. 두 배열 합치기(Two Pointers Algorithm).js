978fdbcf5865f4de0c83fe4b42eaf11fbcd9616f
EunBin Hyun <dmsqlsgus@naver.com>
2021-06-02 20:57:38 +0900

    Create 1. 두 배열 합치기(Two Pointers Algorithm).js

---
+++ 자바스크립트 알고리즘 문제풀이/섹션5/1. 두 배열 합치기(Two Pointers Algorithm).js
@@ -0,0 +1,20 @@
+  function solution(arr1, arr2) {
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = p2 = 0;
+    let answer = [];
+
+    while (p1 < n && p2 < m) {
+      if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+      else answer.push(arr2[p2++]);
+    }
+    while (p1 < n) answer.push(arr1[p1++]);
+    while (p2 < m) answer.push(arr2[p2++]);
+
+    return answer;
+  }
+
+  let arr1 = [1, 3, 5];
+  let arr2 = [2, 3, 6, 7, 9];
+
+  console.log(solution(arr1, arr2));
