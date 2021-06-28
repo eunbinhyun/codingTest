b9bbf56c6e7dcb06b5702478adf0745ac9065b2b
EunBin Hyun <dmsqlsgus@naver.com>
2021-06-28 21:32:54 +0900

    Create  7. 좌표 정렬.js

---
+++ 자바스크립트 알고리즘 문제풀이/섹션7/7. 좌표 정렬.js
@@ -0,0 +1,9 @@
+  function solution(coords) {
+    coords.sort((a, b) => {
+      if (a[0] === b[0]) return a[1] - b[1];
+      else return a[0] - b[0];
+    })
+    return coords;
+  }
+
+  console.log(solution([[2, 7], [1, 3], [1, 2], [2, 5], [3, 6]]));
