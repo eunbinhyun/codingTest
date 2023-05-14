07e79a1f9c94a3397c0baa3d74d1879f754b6bc0
EunBin Hyun <dmsqlsgus@naver.com>
2023-05-14 20:53:46 +0900

    Create 19. Remove Nth Node From End of List.js

---
+++ LeetCode/19. Remove Nth Node From End of List.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  const dummy = new ListNode(0, head);
+  let fast = head;
+  for(let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+  let slow = dummy;
+  while (fast) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return dummy.next;
+};
